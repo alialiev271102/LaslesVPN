d967f641189d304bd68431d55eeb70e7e2e4dac7
Limon <ali.aliev.27.11.02@gmail.com>
2022-10-22 13:36:09 +0600

    Fix spinner

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,12 @@
 let offset = 0;
 const sliderLine = document.querySelector('.coments-block__spiner-line');
 const sliderBox = document.querySelector('.coments-block__spiner-box');
+console.log(sliderBox.offsetWidth);
 const circles = document.querySelectorAll(".circle");
 
 document.querySelector(".next").addEventListener("click", () => {
-    offset += sliderBox.clientWidth;
-    if (offset > sliderBox.clientWidth * 2) {
+    offset += sliderBox.offsetWidth;
+    if (offset > sliderBox.offsetWidth * 2) {
         offset = 0;
     }
     sliderLine.style.left = -offset + "px";
@@ -13,9 +14,9 @@ document.querySelector(".next").addEventListener("click", () => {
 });
 
 document.querySelector(".prev").addEventListener("click", () => {
-    offset -= sliderBox.clientWidth;
+    offset -= sliderBox.offsetWidth;
     if (offset < 0) {
-        offset = sliderBox.clientWidth * 2;
+        offset = sliderBox.offsetWidth * 2;
     }
     sliderLine.style.left = -offset + "px";
     checkCircles();
@@ -28,12 +29,12 @@ const checkCircles = () => {
             circles[1].classList.remove("circle-active");
             circles[2].classList.remove("circle-active");
             break;
-        case sliderBox.clientWidth:
+        case sliderBox.offsetWidth:
             circles[0].classList.remove("circle-active");
             circles[1].classList.add("circle-active");
             circles[2].classList.remove("circle-active");
             break;
-        case sliderBox.clientWidth * 2:
+        case sliderBox.offsetWidth * 2:
             circles[0].classList.remove("circle-active");
             circles[1].classList.remove("circle-active");
             circles[2].classList.add("circle-active");
@@ -45,7 +46,7 @@ checkCircles();
 circles.forEach(elem => {
     elem.addEventListener("click", () => {
         offset = 0;
-        offset = offset + sliderBox.clientWidth * Number(elem.id);
+        offset = offset + sliderBox.offsetWidth * Number(elem.id);
         console.log(offset)
         checkCircles();
         sliderLine.style.left = -offset + "px";
